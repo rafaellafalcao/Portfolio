be37aec713b244dc8ae4ff4ab76250a96690c654
Rafaella Falcão <rafaella.maria@hotmail.com>
2023-10-27 21:41:57 -0300

    commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+import {portfolio} from "./../../data/data.js"
+
+const {username, role, resume, social} = portfolio
+let portfolioName = document.querySelector(".js-portfolio-name")
+let portfolioRole = document.querySelector(".js-portfolio-role")
+let resumeText = document.querySelector(".js-resume-text")
+let portfolioContacts = document.querySelector(".js-portfolio-contacts")
+let btnProjects = document.querySelector(".js-btn-projects")
+let btnModalClose = document.querySelector(".js-modal-close")
+
+portfolioName.innerText = username
+portfolioRole.innerText = role
+resumeText.innerText = resume
+
+for (let i = 0; i < social.length; i++) {
+    let listItem = document.createElement("li")
+    let lisItemImg = document.createElement("img")
+
+    lisItemImg.src = social [i]
+    listItem.appendChild(lisItemImg)
+    portfolioContacts.appendChild(listItem)
+    console.log(social [i])
+}
+
+function openModal () {
+    console.log ("open")
+}
+btnProjects.addEventListener("click", ()=> {
+    document.querySelector(".js-modal").style.display = "inline-block"
+})
+
+btnModalClose.addEventListener("click", ()=> {
+    document.querySelector(".js-modal").style.display = "none"
+})
---
+++ data/data.js
@@ -0,0 +1,19 @@
+const portfolio = {
+    username: "Rafaella Falcão",
+    role: "Software Developer",
+    social: ["./assets/imagens/github.svg", "./assets/imagens/email.svg", "./assets/imagens/linkedin.svg"],
+    resume: "Lorem ipsum dolor sit amet consectetur adipisicing elit." +
+    "Deleniti voluptates odit animi laborum? Quidem, sit assumenda!" +
+    "Temporibus, adipisci est!  Praesentium tenetur nobis illo," +
+    "voluptate neque eum ipsam quas doloribus voluptatibus?",
+    
+    projects: [
+        {name: "", tools: [], url: ""},
+        {name: "", tools: [], url: ""},
+        {name: "", tools: [], url: ""},
+        {name: "", tools: [], url: ""},
+        {name: "", tools: [], url: ""},
+    ]
+}
+
+export {portfolio}
