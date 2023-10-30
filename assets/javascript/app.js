0252a782154787063bd4c85a490f18c793cfcd8d
Rafaella Falcão <rafaella.maria@hotmail.com>
2023-10-30 20:24:40 -0300

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,11 @@
 import {portfolio} from "./../../data/data.js"
-
 const {username, role, resume, social} = portfolio
+
 let portfolioName = document.querySelector(".js-portfolio-name")
 let portfolioRole = document.querySelector(".js-portfolio-role")
 let resumeText = document.querySelector(".js-resume-text")
 let portfolioContacts = document.querySelector(".js-portfolio-contacts")
 let btnProjects = document.querySelector(".js-btn-projects")
-let btnModalClose = document.querySelector(".js-modal-close")
 
 portfolioName.innerText = username
 portfolioRole.innerText = role
@@ -19,12 +18,8 @@ for (let i = 0; i < social.length; i++) {
     lisItemImg.src = social [i]
     listItem.appendChild(lisItemImg)
     portfolioContacts.appendChild(listItem)
-    console.log(social [i])
 }
 
-function openModal () {
-    console.log ("open")
-}
 btnProjects.addEventListener("click", ()=> {
     document.querySelector(".js-modal").style.display = "inline-block"
 })
